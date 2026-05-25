cdd393f549e421f768dfea566646ce73660172de
Raghav <raghavprabhakar5@gmail.com>
2026-05-25 15:50:21 +0530

    optimized build config

--- vortis-vite/vite.config.js
+++ vortis-vite/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -10,4 +9,15 @@ export default defineConfig({
       },
     }),
   ],
+
+  build: {
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 })
