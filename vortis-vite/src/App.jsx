5f9d963ad7b046d3485d9bffc5ed1ffc42b3a39a
Raghav <raghavprabhakar5@gmail.com>
2026-07-08 12:54:49 +0530

    KK

--- vortis-vite/src/App.jsx
+++ vortis-vite/src/App.jsx
@@ -4879,8 +4879,8 @@ return (
   top: '38%',                   
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 800,   // this is what actually makes it look bigger on screen
-  height: 800,
+  width: 500,   // this is what actually makes it look bigger on screen
+  height: 500,
   display: 'flex', 
   alignItems: 'center', 
   justifyContent: 'center',
